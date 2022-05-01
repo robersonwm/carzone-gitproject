01eeb229d9dddfcf8b8312ede9308f4f87fc3027
robersonwm <robersonwm@gmail.com>
2022-05-01 12:53:48 -0400

    nueva carga

--- pages/urls.py
+++ pages/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),       # 'name' permite llamar la url sin agregar la ruta a la etiqueta 'href'
+    path('services', views.services, name='services'),
+    path('contact', views.contact, name='contact'),
 ]
